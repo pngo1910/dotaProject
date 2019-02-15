08263cb87abe248d2bffb78902e5a5ef7fde2990
Phong <thuanphong1910@email.com>
2019-02-15 00:02:44 -0600

    generated teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
   resources :abouts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
