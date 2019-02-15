dcbdad7a78206ba14a4293e4edd406f54907c61c
Phong <thuanphong1910@email.com>
2019-02-14 23:59:06 -0600

    finished about page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_164230) do
+ActiveRecord::Schema.define(version: 2019_02_15_043353) do
 
   create_table "abilities", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_02_14_164230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hero_id"], name: "index_abilities_on_hero_id"
+  end
+
+  create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "heros", force: :cascade do |t|
